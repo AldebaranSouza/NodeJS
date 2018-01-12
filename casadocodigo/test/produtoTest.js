1dcd380295013e175efd802f9379774ba175caca
Aldebaran N. Souza <aldebaran.souza.ext@dasa.com.br>
2018-01-12 14:28:19 -0200

    Aldebaran - add class produtoTest.js

---
+++ casadocodigo/test/produtoTest.js
@@ -0,0 +1,5 @@
+describe('# Teste routes/produto.js', function(){
+    it('# Teste do serviço busca prodito metodo GET', function(){
+        console.log("Chamando o serviço produto GET");
+    });
+});
